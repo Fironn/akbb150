9dd7ab7af0d50fa84401e0015b790daf41a27d12
Fironn <shihomihashi@gmail.com>
2019-06-22 15:00:44 +0900

    [ADD]setting fire base

--- foodmanagement/app/src/main/java/com/example/i80486/foodmanagement/MainActivity.java
+++ foodmanagement/app/src/main/java/com/example/i80486/foodmanagement/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.i80486.foodmanagement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

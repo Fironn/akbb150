8f8874248d9b1dd951d3de2158b404de005bc981
i80486dx2 <i80486dx23419@gmail.com>
2019-06-22 15:31:14 +0900

    sql

--- foodmanagement/app/src/main/java/com/example/i80486/foodmanagement/MainActivity.java
+++ foodmanagement/app/src/main/java/com/example/i80486/foodmanagement/MainActivity.java
@@ -1,19 +1,107 @@
-package com.example.i80486.foodmanagement;
-
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.firebase.analytics.FirebaseAnalytics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAnalytics mFirebaseAnalytics;
+    private TextView textView;
+    private EditText editTextKey, editTextValue;
+    private TestOpenHelper helper;
+    private SQLiteDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextKey = findViewById(R.id.edit_text_key);
+        editTextValue = findViewById(R.id.edit_text_value);
+
+        textView = findViewById(R.id.text_view);
+
+        Button insertButton = findViewById(R.id.button_insert);
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(helper == null){
+                    helper = new TestOpenHelper(getApplicationContext());
+                }
+
+                if(db == null){
+                    db = helper.getWritableDatabase();
+                }
+
+                String key = editTextKey.getText().toString();
+                String value = editTextValue.getText().toString();
+
+                insertData(db, key, Integer.valueOf(value));
+            }
+        });
+
+        Button readButton = findViewById(R.id.button_read);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readData();
+            }
+        });
+    }
+
+
+    private void readData(){
+        if(helper == null){
+            helper = new TestOpenHelper(getApplicationContext());
+        }
+
+        if(db == null){
+            db = helper.getReadableDatabase();
+        }
+        Log.d("debug","**********Cursor");
+
+        Cursor cursor = db.query(
+                "testdb",
+                new String[] { "company", "stockprice" },
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append(": ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("\n");
+            cursor.moveToNext();
+        }
+
+        // 忘れずに！
+        cursor.close();
+
+        Log.d("debug","**********"+sbuilder.toString());
+        textView.setText(sbuilder.toString());
+    }
+
+    private void insertData(SQLiteDatabase db, String com, int price){
+
+        ContentValues values = new ContentValues();
+        values.put("company", com);
+        values.put("stockprice", price);
+
+        db.insert("testdb", null, values);
     }
 }
